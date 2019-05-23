b58e92e7795e340d1f3b5fae163ed6982d58c21d
Nik Gil <ngilevskiy@gmail.com>
2019-05-23 18:00:57 -0400

    Try fix NLTK error

--- pickle_files.py
+++ pickle_files.py
@@ -1,9 +1,11 @@
+import nltk
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from kernels.linear_svc import LinearSVC
 from kernels.naive_bayes import NaiveBayes
 from kernels.log_regression import LogisticRegression
 
+nltk.download('stopwords')
 df = pd.read_csv("training_files/train.csv", encoding="ISO-8859-1")
 train, test = train_test_split(df, test_size=0.33, shuffle=True)
 
